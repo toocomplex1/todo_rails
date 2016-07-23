cec01150e06b85f9c7e732f38d07a9a7558a0781
akash mishra <akash.m@practo.com>
2016-07-23 19:18:55 +0530

    added update delete signin and signup

--- todo/app/models/task.rb
+++ todo/app/models/task.rb
@@ -1,3 +1,17 @@
 class Task < ActiveRecord::Base
+
+  belongs_to :user
   
+  validates_presence_of :user
+  validates_presence_of :title
+  validate :future_completed_date
+  
+
+  private
+
+  def future_completed_date
+    if !completed.blank? && completed > Date.today
+      self.errors.add(:completed, "can't be in the future")
+    end
+  end
 end
